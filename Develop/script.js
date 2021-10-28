8c79b88c9adc6b9bd0bb65a7200e8be77cfd6ea2
michelle <michellenijjar@hotmail.com>
2021-10-28 14:00:47 -0400

    final commit added readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 //What to Do:
-// 1. Add Date
-// 3. Colour code the timeblocks made in html
-// 4. Save in local storage when sumbit button clicked
+// Add Date
+// Colour code the timeblocks made in html
+// Local storage 
 
 $('.saveBtn').click(function (event) {
     var text = $(event.target).prev().val();
@@ -13,49 +13,8 @@ $('.saveBtn').click(function (event) {
     localStorage.setItem(dateKey, text);
 })
 
-// var grabButton = document.querySelector('.saveBtn');    
-// grabButton.onclick=function(){
-//     var textBoxes = {
 
-//     }
-//     console.log(textBoxes);
-//     textBox.push(textBoxes);
-//     localStorage.setItem("textBox",JSON.stringify(textBoxes));
-//     getTextBoxes();
-// }
-
-
-// const storageInput = document.querySelector(".col-md-10");
-// const textType = document.querySelector(".textBox");
-// const saveButton = document.querySelector(".saveBtn");
-
-// const savLocalStorage = () => {
-//     localStorage.setItem('textinput', 'storageInput');
-// }
-
-// saveButton.addEventListener('click', savLocalStorage);
-
-
-
-
-
-// const addtaskinput = document.getElementsByClassName("time-block");
-// const addtaskbtn = document.getElementsByClassName("saveBtn");
-
-// addEventListener.addtaskbtn("click", function(){
-//     addtaskinputval = addtaskinput.value;
-//     let webtask = localStorage.getItem("localtask");
-//     if (webtask == null){
-//         taskObj = [];
-//     }
-//     else{
-//         taskObj = JSON.parse(webtask);
-//     }
-//     taskObj.push(addtaskinputval);
-//     localStorage.setItem("localtask", JSON.stringify(taskObj));
-// })
-
-// 1. Adding Date
+// Adding Date
 
 function setDate() {
     $("#currentDay").html(moment().format('dddd, MMMM Do YYYY, h:mm a'));
@@ -64,7 +23,7 @@ setDate();
 
 
 
-// 3. Colour-coding blocks
+// Colour-coding blocks
 $(".time-block").each(function () {
     var timeSlot = moment().hour();
     var timeBlock = parseInt($(this).attr("id"));
@@ -85,16 +44,3 @@ $(this).children("textarea").val(localStorage.getItem(timeBlock));
     }
 })
 
-//3. local storage with sumbit button click
-// when sumbit button is clicked, saved on local storage(go over challenge4 and modeule 3 notes)
-
-// })
-
-// $('.saveBtn').click(function(event){
-
-//     var text = $(event.target).prev();
-//     var data = text.data().time;
-//     console.log(text.val(), text.data().time);
-
-//     localStorage.setItem(data, text.val());
-// })
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Work Day Scheduler Starter Code
+# Third-Party API's Challenge - Work Day Scheduler 
+
+This work day scheduler is designed for individuals with a busy schedule so that they can add their important events to this planner and manage their time more effectively. This scheduler is set up so the user can see the current date and time at the top of the page. When scrolled down, they are presented with time blocks for standard business hours (9am to 5pm) and have the option to view the time blocks for that day. Furthermore, each time block is colour-coded to indicate whether it is in the past, present, or future. Lastly, the events the user enters are saved in local storage so when the page is refreshed, the saved events do not go away!
+
+
+## Screenshots of the Work Day Scheduler
+
+![workday-scheduler](https://user-images.githubusercontent.com/87215165/139309875-f66ea000-ac32-4fa7-81c6-8b2470e98cb6.png)
+
+![local-storage](https://user-images.githubusercontent.com/87215165/139309869-86b9b19d-7a9f-48bc-893f-8559bd5eab2b.png)
+
+
+
+## Links
+Deployed Link:
+https://mnijjar24.github.io/workday-scheduler/
+
+GitHub Repository Link:
+https://github.com/mnijjar24/workday-scheduler
